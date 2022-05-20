b65e071433e24457ba4d97dfde5478bf9e84856a
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-20 11:57:23 -0300

    feat: initial structure

---
+++ index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import router from './src/routes.js';
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(cors());
+app.use(router);
+
+app.listen(3333, () => {
+  console.log(`Backend is running in localhost:${port}...`);
+});
