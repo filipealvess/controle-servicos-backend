bb6ff536c0969a7c0b49bd336740a32433e72b92
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-05-21 18:54:08 -0300

    feat: add new provider route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { create, login } from './controllers/userController.js';
+import { create as createUser, login } from './controllers/userController.js';
+import { create as createProvider } from './controllers/providerController.js';
 
 const router = express.Router();
 
-router.post('/users', create);
+router.post('/users', createUser);
 router.post('/users/login', login);
+router.post('/providers', createProvider);
 
 export default router;
